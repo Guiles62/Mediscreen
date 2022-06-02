ecf230bfae956ffddef2df184d2a7b288952c989
Guiles62 <https://github.com/Guiles62/P4.git>
2022-06-02 09:58:04 +0200

    Add Config class for Mongo

--- history/src/main/java/com/oc/history/HistoryApplication.java
+++ history/src/main/java/com/oc/history/HistoryApplication.java
@@ -2,9 +2,10 @@ package com.oc.history;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class HistoryApplication {
 
     public static void main(String[] args) {
--- mediscreen/src/main/java/com/oc/mediscreen/MediscreenApplication.java
+++ mediscreen/src/main/java/com/oc/mediscreen/MediscreenApplication.java
@@ -3,9 +3,11 @@ package com.oc.mediscreen;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableSwagger2
 public class MediscreenApplication {
 
     public static void main(String[] args) {
